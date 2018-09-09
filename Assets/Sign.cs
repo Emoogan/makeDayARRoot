51d0507febe76836dc4f8d1649e01de5df1c03b6
unknown <von.poser.m@gmail.com>
2018-09-09 17:41:16 +0200

    first commit

---
+++ Assets/Sign.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sign : MonoBehaviour {
+
+    public ParticleSystem spray;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void startWriting()
+    {
+        if (!spray.isPlaying)
+        {
+            spray.Play();
+            //this.ParticleSystem.Play();
+            Debug.Log("jshdgfu");
+        }
+    }
+
+    public void stopWriting()
+    {
+        if (spray.isPlaying)
+        {
+            spray.Stop();
+            //this.ParticleSystem.Stop();
+            Debug.Log("shbjcdie");
+        }
+    }
+}
---
+++ Assets/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class move : MonoBehaviour {
+
+    public Rigidbody contract;
+    public  Text buttonText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void moveForward()
+    {
+        if (contract != null) { 
+        contract.velocity = Vector3.left * 12f;
+        Object.Destroy(contract, 2f);
+        buttonText.text = "Sign Now"; }
+   
+    }
+
+    /*private void callApi()
+    {
+       // string auth - api_key + ": "
+    }*/
+}
